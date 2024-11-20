08723a50b2cacc44fb0988d3e68ad175d27b6839
rediska123 <larini2004@yandex.ru>
2024-11-20 13:49:31 +0300

    Переделал для PWA

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -1,20 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-const host = process.env.TAURI_DEV_HOST;
-
+// https://vite.dev/config/
 export default defineConfig({
-  base: "/rip_node",
   plugins: [react()],
-  // prevent vite from obscuring rust errors
-  clearScreen: false,
-  server: {
-    // Tauri expects a fixed port, fail if that port is not available
-    strictPort: true,
-    // if the host Tauri is expecting is set, use it
-    host: host || false,
-    port: 5173,
+  base: "/rip_node", // Замените RepoName на имя вашего репозитория
+  server: { 
+    port: 3000,
     proxy: {
       "/api": {
         target: "http://localhost:8000",
@@ -23,17 +15,4 @@ export default defineConfig({
       },
     }
    },
-  // Env variables starting with the item of `envPrefix` will be exposed in tauri's source code through `import.meta.env`.
-  envPrefix: ['VITE_', 'TAURI_ENV_*'],
-  build: {
-    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
-    target:
-      process.env.TAURI_ENV_PLATFORM == 'windows'
-        ? 'chrome105'
-        : 'safari13',
-    // don't minify for debug builds
-    minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
-    // produce sourcemaps for debug builds
-    sourcemap: !!process.env.TAURI_ENV_DEBUG,
-  },
-});
+})

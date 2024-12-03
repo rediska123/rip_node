d5384f3a434d6418f77e44fa9be34a1579d17031
rediska123 <larini2004@yandex.ru>
2024-12-04 01:50:53 +0300

    переделал отображение картинок

--- my-app/src/components/PassCard.tsx
+++ my-app/src/components/PassCard.tsx
@@ -2,7 +2,6 @@ import { FC } from 'react'
 import { Button, Card } from 'react-bootstrap'
 import defaultImage from "../bus.png";
 import "../index.css"
-import {dest_img} from "../../target_config"
 
 
 interface Props {
@@ -16,7 +15,7 @@ interface Props {
 
 const PassCard: FC<Props> = ({ name, description, price, image, imageClickHandler }) => (
     <Card className="card pass-card h-100">
-        <Card.Img className="bd-placeholder-img card-img-top pass-img mx-auto d-block" variant="top" src={(dest_img + image)  || defaultImage} onClick={imageClickHandler}/>
+        <Card.Img className="bd-placeholder-img card-img-top pass-img mx-auto d-block" variant="top" src={image  || defaultImage} onClick={imageClickHandler}/>
         <Card.Body>
             <Card.Title>{name}</Card.Title>
             <Card.Text>{description}</Card.Text>

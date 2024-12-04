150094983bbc7441c37137623ba89926b55ff33b
rediska123 <113539000+rediska123@users.noreply.github.com>
2024-12-04 16:09:52 +0300

    Update vite.config.ts

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  server: { port: 3000 },
   plugins: [react()],
-})
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://192.168.48.1:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});

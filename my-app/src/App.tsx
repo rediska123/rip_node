d6a2e7bfb87914acf88abec99d55710bc4069dd3
rediska123 <larini2004@yandex.ru>
2024-12-18 18:14:35 +0300

    изменил путь из-за изменения названия репозитория

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -6,7 +6,7 @@ import { ROUTES } from "./Routes";
 
 function App() {
   return (
-    <BrowserRouter basename="/rip_node"> {/* RepoName - название вашего репозитория */}
+    <BrowserRouter basename="/passes_frontend"> {/* RepoName - название вашего репозитория */}
       <Routes>
         <Route path={ROUTES.HOME} index element={<HomePage />} />
         <Route path={ROUTES.PASSES} element={<PassesPage />} />
--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/rip_node", // Замените RepoName на имя вашего репозитория
+  base: "/passes_frontend", // Замените RepoName на имя вашего репозитория
   server: {
     port: 3000,
     host: "0.0.0.0",
@@ -13,11 +13,6 @@ export default defineConfig({
         target: 'http://192.168.48.1:8000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-      '/logo': {
-        target: 'http://192.168.48.1:9000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/logo/, ''),
       },
     },
   },

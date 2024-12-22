7cc6cc19a872ae820e6ada6622f6ed118edbbb03
rediska123 <larini2004@yandex.ru>
2024-12-22 22:33:48 +0300

    новая локальная сеть

--- my-app/target_config.ts
+++ my-app/target_config.ts
@@ -1,7 +1,7 @@
 const target_tauri = false
 
-export const api_proxy_addr = "http://172.24.128.1:8000"
-export const img_proxy_addr = "http://172.24.128.1:9000"
+export const api_proxy_addr = "http://192.168.100.2:8000"
+export const img_proxy_addr = "http://192.168.100.2:9000"
 export const dest_api = (target_tauri) ? api_proxy_addr : "api"
 export const dest_img =  (target_tauri) ?  img_proxy_addr : "logo"
 export const dest_root = (target_tauri) ? "" : "/rip_node"
